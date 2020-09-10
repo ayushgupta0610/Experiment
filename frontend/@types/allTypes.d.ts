537d0b82941b8d0ccc447a5c136b2dc01c95a376
Ayush Gupta <ayushgupta0610@gmail.com>
2020-09-10 23:29:21 +0530

    CRA setup

--- frontend/@types/allTypes.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'date-arithmetic';
-declare module 'react-big-calendar';
-declare module '@glidejs/glide';
-declare module 'react-reveal/Fade';
-declare module 'react-anchor-link-smooth-scroll';
